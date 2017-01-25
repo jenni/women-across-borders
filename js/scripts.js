269b695942ece041eccc1cdbebde04d7e142be0e
Jen <jvarelalijo@gmail.com>
2017-01-25 10:59:32 +0100

    layout 2.0

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,12 @@ $(document).ready(function() {
   $('.up').waypoint(function() {
     $(this.element).addClass('visible animated fadeInUp');
   },
-  { offset: '60%' });
+  { offset: '80%' });
 
   $('.down').waypoint(function() {
     $(this.element).addClass('visible animated fadeInDown');
   },
-  { offset: '60%' });
+  { offset: '80%' });
 
   $('.left').waypoint(function() {
     $(this.element).addClass('visible animated fadeInLeft');
@@ -21,11 +21,31 @@ $(document).ready(function() {
   },
   { offset: '80%' });
 
-  /* $('.parallax-window').parallax({imageSrc: 'img1.jpg'}); */
-  $(".nav a").on("click", function(){
-    $(".nav").find(".active").removeClass("active");
-    $(this).parent().addClass("active");
+  /* AUTO SCROLL
+  $(window).load(function () {
+    //normally you'd wait for document.ready, but you'd likely to want to wait
+    //for images to load in case they reflow the page
+    $('body').delay(2000) //wait 5 seconds
+    .animate({
+      //animate jQuery's custom "scrollTop" style
+      //grab the value as the offset of #second from the top of the page
+      'scrollTop': $('#second').offset().top
+    }, 800); //animate over 300ms, change this to however long you want it to animate for
   });
-
-
+*/
+  /* SMOOTH SCROLLING */
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 800);
+          return false;
+        }
+      }
+    });
+  });
 });
